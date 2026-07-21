633d63b7d1c95826f5e25443e071845c51f98437
sondos mohammed <sondos.mohammed0100@gmail.com>
2026-07-21 18:25:21 +0300

    Add Checkov scan

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+const helmet = require("helmet");
 
+app.use(helmet());
 app.get("/", (req, res) => {
     res.json({
         message: "DevSecOps API is running 🚀"
