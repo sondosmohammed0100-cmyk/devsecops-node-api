7584144ee065f21894910d3b6833fff44233ee36
sondos mohammed <sondos.mohammed0100@gmail.com>
2026-07-21 17:18:26 +0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
